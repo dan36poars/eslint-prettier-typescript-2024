3298b2a87bfd8aa36637dd5a8fc4d87367581469
daniel correa <eldestemido@gmail.com>
2024-01-17 22:24:41 -0300

    feat: devgeekweb countdown web app

--- projects/devgeekweb/javascript.js
+++ projects/devgeekweb/javascript.js
@@ -1,3 +1,19 @@
 const countdown = document.querySelector(".countdown");
 
-console.log(countdown);
+
+const updateLabel = (number) => countdown.innerHTML = number;
+
+
+(function () {
+    let number = 10;
+
+    setInterval(() => {
+        number--;
+        updateLabel(number);            
+        if (number < 0) {
+            updateLabel('fim!')
+            number = 10;
+            clearInterval();
+        } 
+    }, 1000);
+})()
